87ddafc26f315aeb8c3ba6b51d7b61b260ab0d02
claudiakemi <claudiakemis@gmail.com>
2019-08-28 23:47:58 -0300

    mudanças no cipher.js, simplificação da fórmula, cifra e decifra tudo

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 ##Produto: DesOcult
 
-Usuários:
-1-Pessoas que têm dificuldade de memorizar senhas e necessitam anotá-las em algum lugar;
-2-Pessoas que, por algum motivo, precisem compartilhar alguma senha com outra pessoa;
-3-Pessoas que queiram criar senhas mais difíceis.
+###Usuários:
 
-Objetivos do usuário em relação ao produto:
--Transformar suas senhas em outros caracteres para "ocultá-las". A senha "ocultada" pode ser anotada sem que outras pessoas descubram e também compartilhada, caso seja necessário. O usuário também pode utilizar o DesOcult para criar senhas mais complexas.
+1.Pessoas que têm dificuldade de memorizar senhas e necessitam anotá-las em algum lugar;
+2.Pessoas que, por algum motivo, precisem compartilhar alguma senha com outra pessoa;
+3.Pessoas que queiram criar senhas mais difíceis.
 
-O produto DesOcult
+###Objetivos do usuário em relação ao produto:
+
+Transformar suas senhas em outros caracteres para "ocultá-las". A senha "ocultada" pode ser anotada sem que outras pessoas descubram e também pode ser compartilhada, caso seja necessário. O usuário também pode utilizar o DesOcult para criar senhas mais complexas.
--- src/cipher.js
+++ src/cipher.js
@@ -5,41 +5,57 @@ window.cipher = {
 };
 
 function encode(offset, inputEncode) {
-  let calculusResult=[];
+  let criptoPassword ="";
   for (let i = 0; i < inputEncode.length; i++) {
-    calculusResult.push(((inputEncode.charCodeAt(i)-65+offset)%26)+65);
+    if (inputEncode.charCodeAt(i) >= 65 && inputEncode.charCodeAt(i) <= 90) {
+      criptoPassword+= String.fromCharCode(((inputEncode.charCodeAt(i)-65+offset)%26)+65);
+    }
+    if (inputEncode.charCodeAt(i) >= 97 && inputEncode.charCodeAt(i) <= 122) {
+      criptoPassword+= String.fromCharCode(((inputEncode.charCodeAt(i)-97+offset)%26)+97);
+    }
+    if (inputEncode.charCodeAt(i) >= 32 && inputEncode.charCodeAt(i) <= 47) {
+      criptoPassword+=String.fromCharCode(((inputEncode.charCodeAt(i)-32+offset)%26)+32);
+    }
+    if (inputEncode.charCodeAt(i) >= 48 && inputEncode.charCodeAt(i) <= 57) {
+      criptoPassword+=String.fromCharCode(((inputEncode.charCodeAt(i)-48+offset)%26)+48);
+    }
+    if (inputEncode.charCodeAt(i) >= 58 && inputEncode.charCodeAt(i) <= 64) {
+      criptoPassword+=String.fromCharCode(((inputEncode.charCodeAt(i)-58-offset)%26)+58);
+    }
+    if (inputEncode.charCodeAt(i) >= 91 && inputEncode.charCodeAt(i) <= 96) {
+      criptoPassword+=String.fromCharCode(((inputEncode.charCodeAt(i)-91+offset)%26)+91);
+    }
+    if (inputEncode.charCodeAt(i) >= 123 && inputEncode.charCodeAt(i) <= 255) {
+      criptoPassword+=String.fromCharCode(((inputEncode.charCodeAt(i)-123+offset)%26)+123);
+    }
   }
-
-  let password=[];
-  for (let i = 0; i < calculusResult.length; i++) {
-    password.push(String.fromCharCode(calculusResult[i]));
-  }
-
-  let criptoPassword = "";
-  for (let i = 0; i < password.length; i++) {
-    criptoPassword=criptoPassword+password[i];
-  }
-
   return criptoPassword;
-
 }
 
 function decode(offset, inputDecode) {
-  let calculusResult=[];
+  let passwordRevealed="";
   for (let i = 0; i < inputDecode.length; i++) {
-    calculusResult.push(((inputDecode.charCodeAt(i)-90-offset)%26)+90);
+    if (inputDecode.charCodeAt(i) >= 65 && inputDecode.charCodeAt(i) <= 90) {
+      passwordRevealed+= String.fromCharCode(((inputDecode.charCodeAt(i)-90-offset)%26)+90);
+    }
+    if (inputDecode.charCodeAt(i) >= 97 && inputDecode.charCodeAt(i) <= 122) {
+      passwordRevealed+=String.fromCharCode(((inputDecode.charCodeAt(i)-122-offset)%26)+122);
+    }
+    if (inputDecode.charCodeAt(i) >= 32 && inputDecode.charCodeAt(i) <= 47) {
+      passwordRevealed+=String.fromCharCode(((inputDecode.charCodeAt(i)-47-offset)%26)+47);
+    }
+    if (inputDecode.charCodeAt(i) >= 48 && inputDecode.charCodeAt(i) <= 57) {
+      passwordRevealed+=String.fromCharCode(((inputDecode.charCodeAt(i)-57-offset)%26)+57);
+    }
+    if (inputDecode.charCodeAt(i) >= 58 && inputDecode.charCodeAt(i) <= 64) {
+      passwordRevealed+=String.fromCharCode(((inputDecode.charCodeAt(i)-64-offset)%26)+64);
+    }
+    if (inputDecode.charCodeAt(i) >= 91 && inputDecode.charCodeAt(i) <= 96) {
+      passwordRevealed+=String.fromCharCode(((inputDecode.charCodeAt(i)-96-offset)%26)+96);
+    }
+    if (inputDecode.charCodeAt(i) >= 123 && inputDecode.charCodeAt(i) <= 255) {
+      passwordRevealed+=String.fromCharCode(((inputDecode.charCodeAt(i)-255-offset)%26)+255);
+    }
   }
-
-  let password=[];
-  for (let i = 0; i < calculusResult.length; i++) {
-    password.push(String.fromCharCode(calculusResult[i]));
-  }
-
-  let passwordRevealed = "";
-  for (let i = 0; i < password.length; i++) {
-    passwordRevealed=passwordRevealed+password[i];
-  }
-
   return passwordRevealed;
-
 }
